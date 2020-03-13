38a28bf5a2c4b6a17ae82d6e3d5f6ae6e5ffa5e5
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-03-13 13:48:30 +0530

    Data

--- chemicalLab/Lab/urls.py
+++ chemicalLab/Lab/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('test/', views.send_data, name = 'test')
 ]
--- chemicalLab/Lab/views.py
+++ chemicalLab/Lab/views.py
@@ -1,5 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import thingspeak
+import random
+import requests
+import urllib
+import http.client as http
+import time
+import json
 def index (request):
     return HttpResponse('hi')
+
+def send_data(request):
+    
+    headers = {
+     "Content-type" : "application/json"   
+    }
+    
+    dictData = {
+        'field1': str(random.randrange(25,30)), 
+        'field2' : str(random.randrange(50,80)),
+        'field3': str(random.randrange(0,500)),
+        'field4': str(random.randrange(500, 510))
+        }
+    
+    requests.post("https://api.thingspeak.com/update?api_key=RYDIXALDBHZQKJOD",data = dictData)
+    
+    request = requests.get("https://api.thingspeak.com/channels/1017900/feeds.json?api_key=CQ98H95JG2IBYHNH")
+    return HttpResponse(request.text)
+# def dummy_data(request):
+#     dictData = {
+#         'Temperature': str(random.randrange(25,30)), 
+#         'Humidity' : str(random.randrange(50,80)),
+#         'AQI': str(random.randrange(0,500)),
+#         'Lab Number': str(random.randrange(500, 510))
+#         }
+#     data = json.dumps(dictData)
+#     # return(HttpResponse(dictData.items()))
+    
+#     url = "https://api.thingspeak.com/update?api_key=RYDIXALDBHZQKJOD"
+        
+#     channel = thingspeak.Channel(
+#         1017900,
+#         api_key = 'RYDIXALDBHZQKJOD',
+#         fmt = 'json',
+#         timeout = None,
+#         server_url = 'https://api.thingspeak.com/'
+#     )
+    
+#     channel.update(data=data)
+
+#     return HttpResponse(channel.get())
