a251a2969001c7828ca2cfcfe0f7a991a3d76114
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-04-08 20:44:12 +0530

    Changes made for ayesha

--- chemicalLab/labAssistent/urls.py
+++ chemicalLab/labAssistent/urls.py
@@ -1,7 +1,33 @@
 from django.urls import path
 from . import views
 
+"""
+Important things in urls.py
+
+a. Map an exisiting views function to the path which means 
+    127.0.0.1:8000/incharge/ access dashboard() in views
+    127.0.0.1:8000/incharge/schedule access schedule() in views
+
+b.To send data via a URL aka URL parameter
+    path("path_name/<datatype:var_name>" , views.get_function(),name = "some_test_URL"),
+    
+    and in views.py :
+    def function_name(request,var_name):
+    
+    should be the function definition
+    
+    Example of a path:
+    path("get_user_details/<int:id>",views.get_user_details , name = "get_user_details")
+    
+    and views function :
+    def get_user_details(request,id):
+    
+    to see how to actually pass content in the URL see the documentation in incharge/boilerplate.html
+"""
+
+
 urlpatterns = [
     path("", views.dashboard, name='incharge_dashboard'),
     path("schedule", views.schedule, name='incharge_schedule'),
+    path("analysis" , views.analysis, name = "incharge_analysis"),
 ]
--- chemicalLab/labAssistent/views.py
+++ chemicalLab/labAssistent/views.py
@@ -1,11 +1,51 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , JsonResponse
+from Lab import models as m
 # Create your views here.
+
+"""
+Certain key functions that you can use in backend coding.
+
+1. For Rendering content , return Jsons
+    a. render(request,'template_name.html',data) :- data has to be a dictionary, Sometimes the dictionary doesnt get passed and the solution to that 
+    is generally to pass {"data" : data} instead of data.
+    
+    b.HttpResponse("html code goes here") :- renders whatever html code you right on the same URL as you operating on (useful for testing )
+    
+    c. JsonResponse(dictionary , safe=False):- we send a dictionary as a JSON but it really is not one and hence passing safe=False is essential,
+                                                to avoid this , use json module and json.dumps() to convert it into a JSON and send the dictionary
+                                                as it is.
+                                                
+2. Models 
+    a. Models have been imported as 'm'. To access models , m.Modelname
+    b. To access data from a model we generally use the following line :
+        var_name = m.ModelName.objects.get() for one object return
+        var_name = m.ModelName.objects.all() for all values in the table
+        var_name = m.ModelName.objects.filter(condition).all() for conditional select
+    
+    c. To access the values within the retrieved model , 
+        Case 1: Single Object aka get()
+            var_name.attr name helps to access the variable.
+        
+        Case 2: Multiple objects aka all() or filter().all()
+            It returns an array of objects of model , to access content within them , send the array in a for loop and access as :
+            
+            for var in var_name:
+                var.attr to access variable
+    
+    d. Alternative way to access data in a model is to convert the object to a dictionary , to do that :
+        var_name = var_name.__dict__
+
+3. API calls to other sites from backend(highly unlikely to be required but still ):
+    Using urllib2 library , documentation is easy to understand.
+"""
 
 
 def dashboard(request):
     return render(request, 'incharge/dashboard.html')
 
-
 def schedule(request):
     return render(request, 'incharge/schedule.html')
+
+def analysis(request):
+    return render(request , "incharge/analysis.html")
