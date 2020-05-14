62e030b88b2c0b51e30f456a5fd09c7b74c9fdd2
jaydbendre <2017.jay.bendre@ves.ac.in>
2020-05-14 13:26:06 +0530

    Regression Complete

--- chemicalLab/labAssistent/views.py
+++ chemicalLab/labAssistent/views.py
@@ -18,32 +18,32 @@ from django.views.decorators.csrf import csrf_exempt
 Certain key functions that you can use in backend coding.
 
 1. For Rendering content , return Jsons
-    a. render(request,'template_name.html',data) :- data has to be a dictionary, Sometimes the dictionary doesnt get passed and the solution to that 
+    a. render(request,'template_name.html',data) :- data has to be a dictionary, Sometimes the dictionary doesnt get passed and the solution to that
     is generally to pass {"data" : data} instead of data.
-    
+
     b.HttpResponse("html code goes here") :- renders whatever html code you right on the same URL as you operating on (useful for testing )
-    
+
     c. JsonResponse(dictionary , safe=False):- we send a dictionary as a JSON but it really is not one and hence passing safe=False is essential,
                                                 to avoid this , use json module and json.dumps() to convert it into a JSON and send the dictionary
                                                 as it is.
-                                                
-2. Models 
+
+2. Models
     a. Models have been imported as 'm'. To access models , m.Modelname
     b. To access data from a model we generally use the following line :
         var_name = m.ModelName.objects.get() for one object return
         var_name = m.ModelName.objects.all() for all values in the table
         var_name = m.ModelName.objects.filter(condition).all() for conditional select
-    
-    c. To access the values within the retrieved model , 
+
+    c. To access the values within the retrieved model ,
         Case 1: Single Object aka get()
             var_name.attr name helps to access the variable.
-        
+
         Case 2: Multiple objects aka all() or filter().all()
             It returns an array of objects of model , to access content within them , send the array in a for loop and access as :
-            
+
             for var in var_name:
                 var.attr to access variable
-    
+
     d. Alternative way to access data in a model is to convert the object to a dictionary , to do that :
         var_name = var_name.__dict__
 
@@ -228,6 +228,10 @@ def reject_request(request, request_id):
     pass
 
 
+lm = LinearRegression()
+scaler = MinMaxScaler()
+
+
 def upload_file(request):
 
     uid = m.User.objects.get(email=request.session["email"])
@@ -254,8 +258,6 @@ def upload_file(request):
         X = np.array(df[independent_var]).reshape(-1, 1)
         y = np.array(df[dependent_var]).reshape(-1, 1)
 
-        scaler = MinMaxScaler()
-
         scaler.fit(X)
 
         X = scaler.transform(X)
@@ -264,7 +266,6 @@ def upload_file(request):
         split = float(request.POST["ratio"])
         X_train, X_test, y_train, y_test = train_test_split(
             X, y, test_size=split, random_state=42)
-        lm = LinearRegression()
 
         model = lm.fit(X_train, y_train)
         pred = lm.predict(X_test)
@@ -301,3 +302,14 @@ def upload_file(request):
         )
     else:
         return HttpResponse("No File")
+
+
+@csrf_exempt
+def predict_single_val(request):
+    global lm, scaler
+
+    data = np.array(float(request.POST["dependent_value"])).reshape(-1, 1)
+    data = scaler.transform(data)
+    predicted_val = lm.predict(data)
+    predicted_val = float(scaler.inverse_transform(predicted_val))
+    return JsonResponse({"Prediction": "{:.4f}".format(predicted_val)})
